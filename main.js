8fde289078b19773d32eba997f55be334148a340
shahjarar <160387866+shahjarar@users.noreply.github.com>
2024-02-29 18:06:01 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// let alien_color:string ="red"
+// if (alien_color == "red")
+// console.log("no output")
+// //task 26
+// let alien_color ="green"
+// if (alien_color == "green") {
+//     console.log("player just earned 5 points for shooting the alien")
+// } else{
+// console.log("player just earned 10 points")
+// }
+var alien_color = "red";
+if (alien_color == "green") {
+    console.log("player just earned 5 points for shooting the alien");
+}
+else {
+    console.log("player just earned 10 points");
+}
